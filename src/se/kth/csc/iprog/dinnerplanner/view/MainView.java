79db7815531c4ff8979284fc6b575360e5fe1b28
jacobhak <jacobhakansson@gmail.com>
2013-01-30 22:28:42 +0100

    Changed view according to lab description

--- src/se/kth/csc/iprog/dinnerplanner/view/MainView.java
+++ src/se/kth/csc/iprog/dinnerplanner/view/MainView.java
@@ -1,14 +1,16 @@
 package se.kth.csc.iprog.dinnerplanner.view;
 
+import se.kth.csc.iprog.dinnerplanner.model.DinnerModel;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
 public class MainView extends JPanel {
 	
-	public MainView(){
+	public MainView(DinnerModel model){
 		JLabel label = new JLabel();
-		label.setText("Hello World");
+		label.setText(""+model.getTotalMenuPrice());
 		this.add(label);
 		
 		//here you set other layout elements
