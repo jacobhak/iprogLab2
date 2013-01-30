43a7c886609a6b00847064c207ff0f8b9a9bf600
jacobhak <jacobhakansson@gmail.com>
2013-01-30 21:29:37 +0100

    Initial commit

---
+++ src/se/kth/csc/iprog/dinnerplanner/DinnerPlanner.java
@@ -0,0 +1,37 @@
+package se.kth.csc.iprog.dinnerplanner;
+
+import javax.swing.JFrame;
+
+import se.kth.csc.iprog.dinnerplanner.model.*;
+import se.kth.csc.iprog.dinnerplanner.view.*;
+
+
+public class DinnerPlanner extends JFrame {
+
+	private DinnerModel model = new DinnerModel();
+
+	public DinnerModel getModel() {
+		return model;
+	}
+
+	public void setModel(DinnerModel model) {
+		this.model = model;
+	}
+
+	public static void main(String[] args) {
+		//Initiating the main JFrame
+		DinnerPlanner dinnerPlanner = new DinnerPlanner();
+		dinnerPlanner.setTitle("Dinner Planner");
+		
+		//Creating the first view
+		MainView mainView = new MainView();
+		
+		//Adding the view to the main JFrame
+		dinnerPlanner.getContentPane().add(mainView);
+		
+		//and starting the JFrame
+		dinnerPlanner.setVisible(true);
+
+	}
+
+}
