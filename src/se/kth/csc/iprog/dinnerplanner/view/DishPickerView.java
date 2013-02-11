832abb070ae53356711ab76359df52e497ae8c52
jsh0rty <jacobhakansson@gmail.com>
2013-02-11 16:37:12 +0100

    Re-made DishPickerView, broken piece of shit

--- src/se/kth/csc/iprog/dinnerplanner/view/DishPickerView.java
+++ src/se/kth/csc/iprog/dinnerplanner/view/DishPickerView.java
@@ -22,45 +22,45 @@ import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 public class DishPickerView extends JPanel{
-	
-	private JTextField searchInput;
-	private JPanel searchBarPanel;
-	private JButton searchButton;
-	private JPanel dishesPanel;
-	private JScrollPane scrollPane;
-	private Set<Dish> dishes;
-	private Set<JButton> dishButtons;
+
+    private JTextField searchInput;
+    private JPanel searchBarPanel;
+    private JButton searchButton;
+    private JPanel dishesPanel;
+    private JScrollPane scrollPane;
+    private Set<Dish> dishes;
+    private Set<JButton> dishButtons;
     private int dishType;
-	
-	
-	public DishPickerView(DinnerModel dm, int dishType) {
-		setLayout(new BorderLayout());
-		dishes = dm.getDishesOfType(dishType);
-		dishButtons = new HashSet<JButton>();
+
+
+    public DishPickerView(DinnerModel dm, int dishType) {
+        setLayout(new BorderLayout());
+        dishes = dm.getDishesOfType(dishType);
+        dishButtons = new HashSet<JButton>();
         this.dishType = dishType;
-		
-		//searchBar panel
-		searchInput = new JTextField();
-		searchInput.setText("Enter dish to search");
-		
-		searchBarPanel = new JPanel(new FlowLayout());
-		searchBarPanel.add(new JLabel("SEARCH: "));
-		searchBarPanel.add(searchInput);
-		searchButton = new JButton("Search");
-		searchBarPanel.add(searchButton);
-		
-		
-		dishesPanel = new JPanel(new GridLayout(2,4));
+
+        //searchBar panel
+        searchInput = new JTextField();
+        searchInput.setText("Enter dish to search");
+
+        searchBarPanel = new JPanel(new FlowLayout());
+        searchBarPanel.add(new JLabel("SEARCH: "));
+        searchBarPanel.add(searchInput);
+        searchButton = new JButton("Search");
+        searchBarPanel.add(searchButton);
+
+
+        dishesPanel = new JPanel(new GridLayout(2,4));
         setDishes(dishes);
-		
-		// Make scrollbar for dishes
-		scrollPane = new JScrollPane(dishesPanel);
-		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
-		
-		// Add this to main layout
-		this.add(searchBarPanel, BorderLayout.PAGE_START);
-		this.add(scrollPane, BorderLayout.CENTER);
-	}
+
+        // Make scrollbar for dishes
+        scrollPane = new JScrollPane(dishesPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+        // Add this to main layout
+        this.add(searchBarPanel, BorderLayout.PAGE_START);
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
 
 //    public JButton getSearchButton(){
 //        return searchButton;
